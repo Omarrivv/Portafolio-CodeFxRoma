3b324edc74b2c176c49630eff66ec152639e2dd4
Omarrivv <omar.rivera@vallegrande.edu.pe>
2024-02-08 23:47:00 -0500

    Portfiolio Actualizado

--- data.js
+++ data.js
@@ -41,21 +41,3 @@ const informacionPersonal = {
     ["instagram", "https://www.youtube.com/@codefxomarroma"],
   ]
 };
-//Función para filtrar las categorías de los trabajos
-function verCategoria(cat){
-    const items = document.getElementsByClassName("item");
-    for(let i=0; i < items.length;i++){
-        items[i].style.display = "none";
-    }
-    const itemCat = document.getElementsByClassName(cat);
-    for(let i = 0; i<itemCat.length;i++){
-        itemCat[i].style.display = "block";
-    }
-    const links = document.querySelectorAll(".trabajos nav a");
-    links[0].className = "";
-    links[1].className = "";
-    links[2].className = "";
-    links[3].className = "";
-    const itemSeleccionado = document.getElementById(cat);
-    itemSeleccionado.className = "borde";
-}
